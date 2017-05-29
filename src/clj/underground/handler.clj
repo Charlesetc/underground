1b3ff90b0027004fb028002d5de3e6559d04ab67
Charles Chamberlain <charlespipin@gmail.com>
2017-05-29 14:57:21 -0500

    Deploying

--- src/clj/underground/handler.clj
+++ src/clj/underground/handler.clj
@@ -10,10 +10,8 @@
 
 (def mount-target
   [:div#app
-      [:h3 "ClojureScript has not been compiled!"]
-      [:p "please run "
-       [:b "lein figwheel"]
-       " in order to start the compiler"]])
+      [:h3#title "the underground narwhal"]
+      ])
 
 (defn head []
   [:head
